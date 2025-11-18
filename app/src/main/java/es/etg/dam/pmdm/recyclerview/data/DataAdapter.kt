3a395763cd3d60a50c929d4de2f5e9f25a0f0b3d
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-11-18 16:05:18 +0100

    Terminado

--- app/src/main/java/es/etg/dam/pmdm/recyclerview/data/DataAdapter.kt
+++ app/src/main/java/es/etg/dam/pmdm/recyclerview/data/DataAdapter.kt
@@ -14,42 +14,33 @@ class DataAdapter(private val mList: List<ItemViewModel>) : RecyclerView.Adapter
     private var onClickListener: OnClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // Cargo el diseño
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.tarjeta_view_design, parent, false)
         return ViewHolder(view)
     }
 
-    // Asociamos un elemento del dataset un viewHolder
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val itemViewModel = mList[position]
 
-        // Cargamos la imagen y descripción de una posición
         holder.imageView.setImageResource(itemViewModel.image)
         holder.textView.text = itemViewModel.descripcion
-
-        // Seteamos el evento para el elemento
         holder.itemView.setOnClickListener {
             onClickListener?.onClick(position, itemViewModel)
         }
     }
 
-    // Obtiene el número de elementos de la lista
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    // Interfaz para el click listener
     interface OnClickListener {
         fun onClick(position: Int, model: ItemViewModel)
     }
 
-    // Set para el atributo
     fun setOnClickListener(listener: OnClickListener?) {
         this.onClickListener = listener
     }
 
-    // Define objetos sobre los elementos de layout
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
         val textView: TextView = itemView.findViewById(R.id.textView)
--- app/src/main/java/es/etg/dam/pmdm/recyclerview/view/MainActivity.kt
+++ app/src/main/java/es/etg/dam/pmdm/recyclerview/view/MainActivity.kt
@@ -2,7 +2,6 @@ package es.etg.dam.pmdm.recyclerview.view
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,32 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Obtengo la view del RecyclerView
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
-
-        // Creamos el dataset
         val data = ArrayList<ItemViewModel>()
+
         for (i in 1..20) {
             val image = android.R.drawable.arrow_up_float
             val descripcion = "Descripción elemento $i"
             data.add(ItemViewModel(image, descripcion))
         }
 
-        // Creamos el adaptador
         val adapter = DataAdapter(data)
-
-        // Configuramos el click listener
         adapter.setOnClickListener(object : DataAdapter.OnClickListener {
             override fun onClick(position: Int, model: ItemViewModel) {
                 val msg: String = "Ha seleccionado el elemento ${model.descripcion}"
                 Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
             }
         })
-
-        // Definimos la gestión del layout
         recyclerview.layoutManager = LinearLayoutManager(this)
-
-        // Establecemos el adaptador al recyclerView
         recyclerview.adapter = adapter
     }
 }
